1b853abd981ebafcd4b80ab7742a0ec712216730
yxzoro <yangxing20107@gmail.com>
2017-12-23 11:13:21 +0800

    -......____.....-

---
+++ haha.js
@@ -0,0 +1,33 @@
+
+
+for (var i = Things.length - 1; i >= 0; i--) {
+    Things[i]
+}
+
+for (var i = 0; i < Things.length; i++) {
+    Things[i]
+}
+
+function function_name(argument) {
+    // body...
+}
+
+
+let a = 1
+
+var b =0
+
+function() {}
+
+export
+
+if (true) {
+
+}
+
+if (true) {
+
+} 
+else {
+
+}    
