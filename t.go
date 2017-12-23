0ba014da574ebb3adaf181ab06aab41103a50f21
yxzoro <yangxing20107@gmail.com>
2017-12-23 10:36:52 +0800

    00000000

--- t.go
+++ t.go
@@ -2,26 +2,14 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"log"
 )
 
 func main() {
-	// never ignore the err !! always check the err and handle the err situation !!
-	f, err := os.Open("haha.txt")
-	if err != nil {
-		fmt.Println("err: ", err)
-		return
-	}
-	fmt.Println("file: ", f)
-
-	/*  if you ignore err, there will be unexpected exception happens in code...
-	f, _ := os.Open("haha.txt")
-	fmt.Println("file: ", f)
-	*/
+	fmt.Print()
 }
 
-func haha(){
-	fmt.Println("----------haha------------")
-	log.Println("------------hehe--------------")	
+
+for {
+	fmt.Println("")
+	
 }
